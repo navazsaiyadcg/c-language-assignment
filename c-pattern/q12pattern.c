0e3004d167a7b465f7751e4226ff5fbcefda8267
Saiyad Navaz <navaz.i.saiyad.cg@gmail.com>
2025-09-15 10:40:06 +0530

    patter q in c language

---
+++ c-pattern/q12pattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+    scanf("%d",&n);
+    for(int i=0; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            printf(" ");
+        }
+        for(int k=0; k<i; k++){
+            printf("%c",'A'+k);
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
